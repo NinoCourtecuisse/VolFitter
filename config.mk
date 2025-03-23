d0f428b75773d51ec7d5c0cc127b09fd7bb37024
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-03-24 00:21:20 +0100

    Add config file

---
+++ config.mk
@@ -0,0 +1,5 @@
+# User-specific configuration
+
+# Modify the path below with the location of your Eigen package
+# Eigen is available at https://eigen.tuxfamily.org/index.php?title=Main_Page
+EIGEN_PATH = /Users/ninocourtecuisse/CPackages/eigen-3.4.0
