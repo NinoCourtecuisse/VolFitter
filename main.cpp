16062cbb2aa0ce6f765c298a0097f01a8cf6a14e
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-04-16 00:09:33 +0200

    Update main and produce plots

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include "DataContainer/Table.h"
 #include "VolSlice/FlatSlice.h"
+#include "VolSlice/SabrSlice.h"
 #include "Utils/Utils.h"
 #include <Eigen/Dense>
+#include <format>
+#include <chrono>
 
 using namespace std;
 using namespace Eigen;
@@ -12,16 +15,33 @@ int main() {
     string fileName("Data/Input/August.csv");
     Table table(fileName);
 
-    double maturity(1.0);
-    FlatSlice slice(maturity);
-    slice.calibrate(table);
+    chrono::year_month_day today{chrono::year{2024}, chrono::month{7}, chrono::day{31}};
+    chrono::year_month_day maturity{chrono::year{2024}, chrono::month{8}, chrono::day{16}};
+    chrono::sys_days d1 = chrono::sys_days{today};
+    chrono::sys_days d2 = chrono::sys_days{maturity};
+    chrono::days delta = d2 - d1;
+    double ttm = delta.count() / 365.25;
 
-    string outputFile("Data/Output/output.csv");
+    double S0 = 5535;
+    double r = 0.05;
+
+    double init_vol = 0.2;
+    FlatSlice flat_slice(ttm, init_vol);
+    flat_slice.calibrate(table, S0, r);
+
+    string outputFile("Data/Output/flat_slice.csv");
     double min_strike = table.getMin(string("Strike"));
     double max_strike = table.getMax(string("Strike"));
-    int N = 10;
+    int N = 30;
     VectorXd K = linspace(min_strike, max_strike, N);
-    slice.save(K, maturity, outputFile);
+    flat_slice.save(K, ttm, S0, r, outputFile);
+
+    Vector4d init_params(0.9, 0.3, -0.5, 1.0);
+    SabrSlice sabr_slice(ttm, init_params);
+    sabr_slice.calibrate(table, S0, r);
+
+    outputFile = "Data/Output/sabr_slice.csv";
+    sabr_slice.save(K, ttm, S0, r, outputFile);
 
     return 0;
 }
--- plot.py
+++ plot.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 input = pd.read_csv('Data/Input/August.csv')
-output = pd.read_csv('Data/Output/output.csv')
+FILENAME = 'sabr_slice'
+output = pd.read_csv('Data/Output/' + FILENAME + '.csv')
 
 plt.plot(output['Strike'], output['IV'], label='model iv', color='blue')
 plt.scatter(input['Strike'], input['IV'], marker='+', s=5.0, label='market iv', color='grey')
 plt.xlabel("Strike")
 plt.ylabel("IV")
 plt.legend()
-plt.savefig('Data/Output/output.png')
+plt.savefig('Data/Output/' + FILENAME + '.png')
