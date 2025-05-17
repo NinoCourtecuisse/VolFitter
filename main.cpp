3285022ccc792a6fcf78e122030e57c310f4fe01
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-05-18 00:08:31 +0200

    Fit all slices

--- main.cpp
+++ main.cpp
@@ -10,38 +10,50 @@
 
 using namespace std;
 using namespace Eigen;
+typedef chrono::year_month_day Ymd;
+typedef vector<Ymd> Dates;
 
 int main() {
-    string fileName("Data/Input/August.csv");
-    Table table(fileName);
+    string file_path = "Data/Input/";
 
-    chrono::year_month_day today{chrono::year{2024}, chrono::month{7}, chrono::day{31}};
-    chrono::year_month_day maturity{chrono::year{2024}, chrono::month{8}, chrono::day{16}};
-    chrono::sys_days d1 = chrono::sys_days{today};
-    chrono::sys_days d2 = chrono::sys_days{maturity};
-    chrono::days delta = d2 - d1;
-    double ttm = delta.count() / 365.25;
-
+    Ymd today{chrono::year{2024}, chrono::month{7}, chrono::day{31}};
     double S0 = 5535;
     double r = 0.05;
 
-    double init_vol = 0.2;
-    FlatSlice flat_slice(ttm, init_vol);
-    flat_slice.calibrate(table, S0, r);
+    vector<string> months({"August", "November", "October", "September"});
+    Dates maturity_dates = {
+        Ymd(chrono::year{2024}, chrono::month{8}, chrono::day{16}),
+        Ymd(chrono::year{2024}, chrono::month{9}, chrono::day{20}),
+        Ymd(chrono::year{2024}, chrono::month{10}, chrono::day{18}),
+        Ymd(chrono::year{2024}, chrono::month{11}, chrono::day{15})
+    };
+    for (size_t i(0); i < months.size(); ++i) {
+        string file_name(file_path + months[i] + ".csv");
+        Table table(file_name);
+        Ymd maturity = maturity_dates[i];
 
-    string outputFile("Data/Output/flat_slice.csv");
-    double min_strike = table.getMin(string("Strike"));
-    double max_strike = table.getMax(string("Strike"));
-    int N = 30;
-    VectorXd K = linspace(min_strike, max_strike, N);
-    flat_slice.save(K, ttm, S0, r, outputFile);
+        chrono::sys_days d1 = chrono::sys_days{today};
+        chrono::sys_days d2 = chrono::sys_days{maturity};
+        chrono::days delta = d2 - d1;
+        double ttm = delta.count() / 365.25;
 
-    Vector4d init_params(0.9, 0.3, -0.5, 1.0);
-    SabrSlice sabr_slice(ttm, init_params);
-    sabr_slice.calibrate(table, S0, r);
+        double init_vol = 0.2;
+        FlatSlice flat_slice(ttm, init_vol);
+        flat_slice.calibrate(table, S0, r);
 
-    outputFile = "Data/Output/sabr_slice.csv";
-    sabr_slice.save(K, ttm, S0, r, outputFile);
+        string outputFile("Data/Output/" + months[i] + "_flat.csv");
+        double min_strike = table.getMin(string("Strike"));
+        double max_strike = table.getMax(string("Strike"));
+        int N = 30;
+        VectorXd K = linspace(min_strike, max_strike, N);
+        flat_slice.save(K, ttm, S0, r, outputFile);
 
+        Vector4d init_params(0.9, 0.3, -0.5, 1.0);
+        SabrSlice sabr_slice(ttm, init_params);
+        sabr_slice.calibrate(table, S0, r);
+
+        outputFile = "Data/Output/" + months[i] + "_sabr.csv";
+        sabr_slice.save(K, ttm, S0, r, outputFile);
+    }
     return 0;
 }
--- plot.py
+++ plot.py
@@ -1,13 +1,26 @@
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import pandas as pd
 
-input = pd.read_csv('Data/Input/August.csv')
-FILENAME = 'sabr_slice'
-output = pd.read_csv('Data/Output/' + FILENAME + '.csv')
+input_path = 'Data/Input/'
+output_path = 'Data/Output/'
+months = ["August", "November", "October", "September"]
 
-plt.plot(output['Strike'], output['IV'], label='model iv', color='blue')
-plt.scatter(input['Strike'], input['IV'], marker='+', s=5.0, label='market iv', color='grey')
+colors = ['red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'black', 'white', 'orange', 'purple', 'pink', 'brown', 'gray', 'olive', 'teal', 'navy']
+for i in range(len(months)):
+    input = pd.read_csv(input_path + months[i] + '.csv')
+    flat_slice = pd.read_csv(output_path + months[i] + '_flat.csv')
+    sabr_slice = pd.read_csv(output_path + months[i] + '_sabr.csv')
+
+    plt.plot(sabr_slice['Strike'], sabr_slice['IV'], color=colors[i], marker='o', markersize=5.0)
+    plt.plot(flat_slice['Strike'], flat_slice['IV'], color=colors[i], marker='*', markersize=5.0)
+    plt.scatter(input['Strike'], input['IV'], color='black', marker='+', s=5.0)
+
 plt.xlabel("Strike")
 plt.ylabel("IV")
-plt.legend()
-plt.savefig('Data/Output/' + FILENAME + '.png')
+
+plus_marker = mlines.Line2D([], [], marker='+', linestyle='None', label='market')
+star_marker = mlines.Line2D([], [], marker='*', linestyle='None', label='flat')
+circle_marker = mlines.Line2D([], [], marker='o', linestyle='None', label='sabr')
+plt.legend(handles=[plus_marker, star_marker, circle_marker])
+plt.savefig('Data/Output/slice.png')
